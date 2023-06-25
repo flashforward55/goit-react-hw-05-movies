c5b4d63809be0bfd0d532664e26adb63a2ef2d13
Anton Styazhkin <rudana089@gmail.com>
2023-06-25 21:40:43 +0300

    Update MovieDetails.jsx

--- src/components/MovieDetails/MovieDetails.jsx
+++ src/components/MovieDetails/MovieDetails.jsx
@@ -2,11 +2,12 @@ import React, { useEffect, useState } from 'react';
 
 const MovieDetails = ({ match }) => {
   const [movieDetails, setMovieDetails] = useState(null);
+  const [cast, setCast] = useState([]);
 
   useEffect(() => {
     const fetchMovieDetails = async () => {
       try {
-        const apiKey = 'YOUR_API_KEY'; // Replace with your actual API key
+        const apiKey = '6d44761072bd3455ede44a6813ca8a9a'; // Replace with your actual API key
         const movieId = match.params.movieId;
         const response = await fetch(
           `https://api.themoviedb.org/3/movie/${movieId}?api_key=${apiKey}`
@@ -21,6 +22,24 @@ const MovieDetails = ({ match }) => {
     fetchMovieDetails();
   }, [match.params.movieId]);
 
+  useEffect(() => {
+    const fetchMovieCredits = async () => {
+      try {
+        const apiKey = 'YOUR_API_KEY'; // Replace with your actual API key
+        const movieId = match.params.movieId;
+        const response = await fetch(
+          `https://api.themoviedb.org/3/movie/${movieId}/credits?api_key=${apiKey}`
+        );
+        const data = await response.json();
+        setCast(data.cast);
+      } catch (error) {
+        console.error('Error fetching movie credits:', error);
+      }
+    };
+
+    fetchMovieCredits();
+  }, [match.params.movieId]);
+
   if (!movieDetails) {
     return <div>Loading...</div>;
   }
@@ -29,6 +48,12 @@ const MovieDetails = ({ match }) => {
     <div>
       <h2>{movieDetails.title}</h2>
       <p>{movieDetails.overview}</p>
+      <h3>Cast:</h3>
+      <ul>
+        {cast.map(actor => (
+          <li key={actor.id}>{actor.name}</li>
+        ))}
+      </ul>
       {/* Render other movie details */}
     </div>
   );
