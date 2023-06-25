bf5730dfc3f9a59c51ed57b1b72e568822f3c752
Anton Styazhkin <rudana089@gmail.com>
2023-06-25 21:38:39 +0300

    Add component Movies

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,12 +1,5 @@
 import React, { lazy, Suspense } from 'react';
-import {
-  BrowserRouter as Router,
-  Route,
-  Routes,
-  /*   Switch, */
-
-  /*   Redirect, */
-} from 'react-router-dom';
+import { BrowserRouter as Router, Route, Routes } from 'react-router-dom';
 
 const Home = lazy(() => import('components/Home/Home'));
 const Movies = lazy(() => import('components/Movies/Movies'));
