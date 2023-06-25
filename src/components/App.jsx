3c6a982a4bdeefe1e2a539638f90d92293f06396
Anton Styazhkin <rudana089@gmail.com>
2023-06-25 21:31:09 +0300

    Add Ruoter componetnts

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,16 +1,36 @@
-export const App = () => {
+import React, { lazy, Suspense } from 'react';
+import {
+  BrowserRouter as Router,
+  Route,
+  Routes,
+  /*   Switch, */
+
+  /*   Redirect, */
+} from 'react-router-dom';
+
+const Home = lazy(() => import('components/Home/Home'));
+const Movies = lazy(() => import('components/Movies/Movies'));
+const MovieDetails = lazy(() => import('components/MovieDetails/MovieDetails'));
+const Cast = lazy(() => import('components/Cast/Cast'));
+const Reviews = lazy(() => import('components/Reviews/Reviews'));
+
+const App = () => {
   return (
-    <div
-      style={{
-        height: '100vh',
-        display: 'flex',
-        justifyContent: 'center',
-        alignItems: 'center',
-        fontSize: 40,
-        color: '#010101'
-      }}
-    >
-      React homework template
-    </div>
+    <Router>
+      <Suspense fallback={<div>Loading...</div>}>
+        <Routes>
+          {/*           <Switch> */}
+          <Route exact path="/" component={Home} />
+          <Route exact path="/movies" component={Movies} />
+          <Route exact path="/movies/:movieId" component={MovieDetails} />
+          <Route exact path="/movies/:movieId/cast" component={Cast} />
+          <Route exact path="/movies/:movieId/reviews" component={Reviews} />
+          {/*  <Redirect to="/" /> */}
+          {/* </Switch> */}
+        </Routes>
+      </Suspense>
+    </Router>
   );
 };
+
+export default App;
---
+++ src/components/Home/Home.jsx
@@ -0,0 +1,39 @@
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+
+const Home = () => {
+  const [movies, setMovies] = useState([]);
+
+  useEffect(() => {
+    const fetchMovies = async () => {
+      try {
+        const response = await axios.get(
+          'https://api.themoviedb.org/3/trending/get-trending',
+          {
+            params: {
+              api_key: '6d44761072bd3455ede44a6813ca8a9a',
+            },
+          }
+        );
+        setMovies(response.data.results);
+      } catch (error) {
+        console.log('Error fetching trending movies:', error);
+      }
+    };
+
+    fetchMovies();
+  }, []);
+
+  return (
+    <div>
+      <h1>Trending Movies</h1>
+      <ul>
+        {movies.map(movie => (
+          <li key={movie.id}>{movie.title}</li>
+        ))}
+      </ul>
+    </div>
+  );
+};
+
+export default Home;
