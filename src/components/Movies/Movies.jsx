bb1237ae64f1a5f818c760034b4aa15a8721a471
Anton Styazhkin <rudana089@gmail.com>
2023-06-26 19:01:54 +0300

    Add style

--- src/components/Movies/Movies.jsx
+++ src/components/Movies/Movies.jsx
@@ -2,6 +2,74 @@ import React, { useState } from 'react';
 import axios from 'axios';
 import Loader from 'components/Loader/Loader';
 import { Link } from 'react-router-dom';
+import styled from 'styled-components';
+
+//import placeholderPoster from 'services/No-Image.svg';
+
+const MoviesContainer = styled.div`
+  background-color: #f9f9f9;
+  padding: 20px;
+`;
+
+const Title = styled.h1`
+  color: #333;
+  margin-bottom: 20px;
+`;
+
+const SearchContainer = styled.div`
+  display: flex;
+  margin-bottom: 20px;
+`;
+
+const SearchInput = styled.input`
+  padding: 8px;
+  font-size: 16px;
+`;
+
+const SearchButton = styled.button`
+  padding: 8px 16px;
+  font-size: 16px;
+  background-color: #333;
+  color: #fff;
+  border: none;
+  cursor: pointer;
+`;
+
+const MovieList = styled.ul`
+  list-style: none;
+  padding: 0;
+  display: flex;
+  flex-wrap: wrap;
+`;
+
+const MovieItem = styled.li`
+  flex: 0 0 calc(100% / 6);
+  margin-bottom: 10px;
+`;
+
+const MovieLink = styled(Link)`
+  color: #333;
+  text-decoration: none;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  text-align: center;
+`;
+
+const MoviePoster = styled.img`
+  width: 150px; /* Increase the width of the image */
+  height: 225px; /* Increase the height of the image */
+  margin-bottom: 5px; /* Reduce the bottom margin */
+`;
+
+const PlaceholderPoster = styled.div`
+  width: 120px;
+  height: 180px;
+  margin-bottom: 10px;
+  background-image: url("data:image/svg+xml;charset=UTF-8,%3c?xml version='1.0' encoding='UTF-8' standalone='no'?%3e%3csvg xmlns='http://www.w3.org/2000/svg' width='225' height='150' version='1.0'%3e%3cpath fill='%23d4d4d4' d='M0 0v150h225V0H0z'/%3e%3cpath fill='%23fff' d='m64.055077 38.894466 36.105543 36.105536-36.105543 36.105538 12.320373 12.32038 36.10554-36.105547 36.12454 36.124547 12.33939-12.33938-36.12454-36.124551 36.10554-36.105537-12.32039-12.320372-36.10552 36.105536L76.394463 26.55508 64.055077 38.894466z' overflow='visible' style='marker:none'/%3e%3c/svg%3e");
+  background-size: cover;
+  background-position: center;
+`;
 
 const Movies = () => {
   const [searchQuery, setSearchQuery] = useState('');
@@ -37,28 +105,40 @@ const Movies = () => {
   };
 
   return (
-    <div>
-      <h1>Search Movies</h1>
-      <input
-        type="text"
-        value={searchQuery}
-        onChange={e => setSearchQuery(e.target.value)}
-        onKeyDown={handleKeyDown}
-      />
-      <button onClick={handleSearch}>Search</button>
+    <MoviesContainer>
+      <Title>Search Movies</Title>
+      <SearchContainer>
+        <SearchInput
+          type="text"
+          value={searchQuery}
+          onChange={e => setSearchQuery(e.target.value)}
+          onKeyDown={handleKeyDown}
+        />
+        <SearchButton onClick={handleSearch}>Search</SearchButton>
+      </SearchContainer>
 
       {isLoading ? (
         <Loader />
       ) : (
-        <ul>
+        <MovieList>
           {searchResults.map(movie => (
-            <li key={movie.id}>
-              <Link to={`/movies/${movie.id}`}>{movie.title}</Link>
-            </li>
+            <MovieItem key={movie.id}>
+              <MovieLink to={`/movies/${movie.id}`}>
+                {movie.poster_path ? (
+                  <MoviePoster
+                    src={`https://image.tmdb.org/t/p/w300/${movie.poster_path}`}
+                    alt={movie.title}
+                  />
+                ) : (
+                  <PlaceholderPoster />
+                )}
+                <span>{movie.title}</span>
+              </MovieLink>
+            </MovieItem>
           ))}
-        </ul>
+        </MovieList>
       )}
-    </div>
+    </MoviesContainer>
   );
 };
 
