e60457d944dd71f24d47347c788a369a60572ce5
Anton Styazhkin <rudana089@gmail.com>
2023-06-26 17:59:21 +0300

    Update Home.jsx

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,7 +1,61 @@
 import React, { useEffect, useState } from 'react';
 import { Link } from 'react-router-dom';
 import PropTypes from 'prop-types';
+import styled from 'styled-components';
 import { fetchTrendingMovies } from 'services/api';
+
+const Container = styled.div`
+  padding: 20px;
+  text-align: center;
+`;
+
+const Title = styled.h1`
+  font-size: 24px;
+  margin-bottom: 20px;
+`;
+
+const MovieList = styled.ul`
+  list-style: none;
+  padding: 0;
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+`;
+
+const MovieItem = styled.li`
+  width: calc(100% / 6);
+  margin-bottom: 20px;
+  padding: 10px;
+
+  @media (max-width: 1200px) {
+    width: calc(100% / 4);
+  }
+
+  @media (max-width: 800px) {
+    width: calc(100% / 3);
+  }
+
+  @media (max-width: 600px) {
+    width: calc(100% / 2);
+  }
+`;
+
+const MovieLink = styled(Link)`
+  display: block;
+  text-decoration: none;
+  color: #333;
+`;
+
+const MovieImage = styled.img`
+  width: 100%;
+  height: auto;
+  margin-bottom: 10px;
+`;
+
+const MovieTitle = styled.p`
+  font-size: 16px;
+  margin: 0;
+`;
 
 const Home = () => {
   const [movies, setMovies] = useState([]);
@@ -16,22 +70,22 @@ const Home = () => {
   }, []);
 
   return (
-    <div>
-      <h1>Trending Movies</h1>
-      <ul>
+    <Container>
+      <Title>Trending Movies</Title>
+      <MovieList>
         {movies.map(movie => (
-          <li key={movie.id}>
-            <Link to={`/movies/${movie.id}`}>
-              <img
+          <MovieItem key={movie.id}>
+            <MovieLink to={`/movies/${movie.id}`}>
+              <MovieImage
                 src={`https://image.tmdb.org/t/p/w500/${movie.poster_path}`}
                 alt={movie.title}
               />
-              <p>{movie.title}</p>
-            </Link>
-          </li>
+              <MovieTitle>{movie.title}</MovieTitle>
+            </MovieLink>
+          </MovieItem>
         ))}
-      </ul>
-    </div>
+      </MovieList>
+    </Container>
   );
 };
 
