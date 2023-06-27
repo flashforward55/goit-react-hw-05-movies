7a4dc1389ecfe6b02e85eb1403cc6bf38f21279d
Anton Styazhkin <rudana089@gmail.com>
2023-06-28 00:03:57 +0300

    Add index page

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -18,11 +18,12 @@ const App = () => {
       </Navigation>
       <Suspense fallback={<Loader />}>
         <Routes>
-          <Route path="/" element={<Home />} />
+          <Route index element={<Home />} />
           <Route path="/movies" element={<Movies />} />
-          <Route path="/movies/:movieId" element={<MovieDetails />} />
-          <Route path="/movies/:movieId/cast" element={<Cast />} />
-          <Route path="/movies/:movieId/reviews" element={<Reviews />} />
+          <Route path="/movies/:movieId" element={<MovieDetails />}>
+            <Route path="cast" element={<Cast />} />
+            <Route path="reviews" element={<Reviews />} />
+          </Route>
           <Route path="*" element={<Home />} />
         </Routes>
       </Suspense>
