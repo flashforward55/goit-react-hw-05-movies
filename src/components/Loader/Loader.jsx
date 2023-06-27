35f97957f8cdea2b14a50094d3e42ba1457b773b
Anton Styazhkin <rudana089@gmail.com>
2023-06-27 12:26:17 +0300

    Update Loader.jsx

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,9 +1,47 @@
 import { Oval } from 'react-loader-spinner';
+import PropTypes from 'prop-types';
 import { LoaderWrapper } from './Loader.styled';
 
-const Loader = () => (
+const Loader = ({
+  type,
+  color,
+  height,
+  width,
+  secondaryColor,
+  strokeWidth,
+  strokeWidthSecondary,
+}) => (
   <LoaderWrapper>
-    <Oval color="#00BFFF" secondaryColor="#00BFFF" height={80} width={80} />
+    <Oval
+      type={type}
+      color={color}
+      secondaryColor={secondaryColor}
+      height={height}
+      width={width}
+      strokeWidth={strokeWidth}
+      strokeWidthSecondary={strokeWidthSecondary}
+    />
   </LoaderWrapper>
 );
+
+Loader.propTypes = {
+  type: PropTypes.string.isRequired,
+  color: PropTypes.string.isRequired,
+  secondaryColor: PropTypes.string.isRequired,
+  height: PropTypes.string.isRequired,
+  width: PropTypes.string.isRequired,
+  strokeWidth: PropTypes.string.isRequired,
+  strokeWidthSecondary: PropTypes.string.isRequired,
+};
+
+Loader.defaultProps = {
+  type: 'TailSpin',
+  color: '#00BFFF',
+  secondaryColor: '#00BFFF',
+  height: '80',
+  width: '80',
+  strokeWidth: '4',
+  strokeWidthSecondary: '4',
+};
+
 export default Loader;
